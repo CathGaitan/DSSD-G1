4c63f86047f66a6902759454dfd01621d5027306
cath <catu.gaitan@gmail.com>
2025-09-24 19:54:56 -0300

    anda login y obtener todos ls procesos

--- backend/app/bonita_integration/bonita_client.py
+++ backend/app/bonita_integration/bonita_client.py
@@ -1,33 +1,37 @@
-import requests
 import os
+import requests
+from app.core.exceptions import BonitaAPIError
 
 
-def login_bonita() -> dict:
-    login_url = os.getenv("BONITA_URL") + "/loginservice"
-    data = {
-        "username": "walter.bates",
-        "password": "bpm",
-        "redirect": "false"
-    }
-    headers = {
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
-    response = requests.post(data=data, url=login_url, headers=headers)
-    if response.status_code != 200:
-        raise Exception(
-            f"Error al loguearse en Bonita:\n"
-            f"Status: {response.status_code}\n"
-            f"Reason: {response.reason}\n"
-            f"URL: {response.url}\n"
-            f"Body: {response.text}\n"
-            f"Headers: {response.headers}"
-        )
-    jsession = response.cookies.get("JSESSIONID")
+class BonitaClient:
+    def __init__(self):
+        self.base_url = os.getenv("BONITA_URL")
+        self.session = requests.Session()
+        self.logged_in = False
 
-    return {
-        "session": jsession,
-        "cookies": response.cookies,
-        "headers": {
-            "Cookie": f"JSESSIONID={jsession}"
+    def login(self):
+        login_url = f"{self.base_url}/loginservice"
+        payload = {
+            "username": "walter.bates",
+            "password": "bpm",
+            "redirect": "false"
         }
-    }
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        response = self.session.post(login_url, data=payload, headers=headers)
+        if response.status_code not in (200, 204):
+            raise BonitaAPIError(status_code=response.status_code, url=response.url, body=response.text, headers=response.headers)
+        self.logged_in = True
+        token = response.cookies.get("X-Bonita-API-Token")
+        if token:
+            self.session.headers.update({"X-Bonita-API-Token": token})
+
+    def is_logged(self):
+        if not self.logged_in:
+            self.login()
+
+    def get_all_processes(self):
+        self.is_logged()
+        url = f"{self.base_url}/API/bpm/process?p=0&c=100"
+        response = self.session.get(url)
+        response.raise_for_status()
+        return response.json()
